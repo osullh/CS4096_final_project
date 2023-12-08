8a7b99783f88e3ede02950bbef63f502aafa8811
Helena O'Sullivan <osullh@rpi.edu>
2023-12-08 17:33:50 +0000

    NavMesh Implementation

--- Final Project/Assets/Player/PlayerJump.cs
+++ Final Project/Assets/Player/PlayerJump.cs
@@ -9,6 +9,7 @@ public class PlayerJump : MonoBehaviour
     InputAction jumpAction;   // Variable for Jump Action
     [SerializeField] Rigidbody rb;    // Variable for the Player Rigidbody
     [SerializeField] float JumpPower; // Variable to Change to Jump Power
+    public bool isGrounded = false;   // Variable to Check if Player is Grounded
 
     private void Awake()
     {
@@ -19,6 +20,7 @@ public class PlayerJump : MonoBehaviour
     private void OnEnable()
     {
         // Let's Call a Jump Event on Action Performed
+        isGrounded = true;
         jumpAction.performed += Jump;
     }
 
@@ -30,9 +32,27 @@ public class PlayerJump : MonoBehaviour
 
     void Jump(InputAction.CallbackContext context)
     {
-        // Debug.Log("Jump"); // DEBUG Statement
+        Debug.Log("Jump Action Called"); // DEBUG Statement
 
-        // Add an upward force to the player to make it jump
-        rb.AddForce(Vector3.up * JumpPower);
+        // Check to see if Player is Grouded - Will Prevent Double Jump
+
+        // If Player is Grouded -> Jump
+        if (isGrounded == true)
+        {
+            Debug.Log("Grounded - Let's Jump"); // DEBUG Statement
+
+            // Add an upward force to the player to make it jump
+            rb.AddForce(Vector3.up * JumpPower);
+            isGrounded = false;
+        }
+        // If Player is Not Grouded -> Return to Ground
+        else
+        {
+            Debug.Log("Grounded - Let's Jump"); // DEBUG Statement
+
+            // Add an downward force to the player so they go down
+            rb.AddForce(Vector3.down * JumpPower);
+            isGrounded = true;
+        }
     }
 }
